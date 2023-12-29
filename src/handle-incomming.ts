b2d864a723e24e49a88bcc2bedd4d30471a23c14
Patrick <87815239+ptrckbp@users.noreply.github.com>
2023-12-29 16:09:52 -0500

    adds validation using zod

---
+++ src/handle-incomming.ts
@@ -0,0 +1,86 @@
+import zod from "zod";
+
+const getInputIssues = (body: any): any[] => {
+  const bodySchema = zod.object({
+    userId: zod.string(),
+    messageId: zod.string(),
+    conversationId: zod.string(),
+    type: zod.string().min(1),
+    text: zod.string().min(1),
+    payload: zod
+      .object({
+        text: zod.string().optional(),
+      })
+      .optional(),
+  });
+
+  try {
+    bodySchema.parse(body);
+    return [];
+  } catch (e) {
+    // return the human readable error
+    return e.issues;
+  }
+};
+
+const handleIncoming = async ({ req, client, ctx, logger }) => {
+  const data = JSON.parse(req.body);
+
+  const { userId, messageId, conversationId, type, text, payload } = data;
+
+  const inputIssues = getInputIssues(data);
+
+  if (inputIssues.length > 0) {
+    return {
+      status: 400,
+      body: "Validation Error! Issues:" + "\n" + JSON.stringify(inputIssues),
+    };
+  }
+
+  const { conversation } = await client.getOrCreateConversation({
+    channel: "channel",
+    tags: {
+      id: conversationId,
+    },
+  });
+
+  const { user } = await client.getOrCreateUser({
+    tags: {
+      id: userId,
+    },
+  });
+
+  const botpressMessage = {
+    tags: {
+      id: messageId,
+    },
+    type,
+    text,
+    userId: user.id,
+    conversationId: conversation.id,
+    payload,
+  };
+
+  if (!payload) {
+    botpressMessage.payload = { text: text };
+  } else if (!payload.text) {
+    botpressMessage.payload.text = text;
+  }
+
+  const { message } = await client.createMessage(botpressMessage);
+
+  logger
+    .forBot()
+    .debug(
+      `Done creating message in botpress for user ${userId}: ${JSON.stringify(
+        payload
+      )}`
+    );
+
+  return {
+    status: 200,
+    body: JSON.stringify({ message, user, conversation }),
+  };
+};
+
+export default handleIncoming;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as botpress from ".botpress";
 import { axios, Conversation } from "@botpress/client";
+import handleIncoming from "./handle-incomming";
 
 const INTEGRATION_NAME = "botpressapi";
 const idTag = `${INTEGRATION_NAME}:id` as const;
@@ -68,62 +69,7 @@ const integration = new botpress.Integration({
       },
     },
   },
-  handler: async ({ req, client, ctx, logger }) => {
-    logger
-      .forBot()
-      .debug(
-        "Handler received request from BotpressApi with payload:",
-        req.body
-      );
-
-    const { userId, messageId, conversationId, type, text, payload } =
-      JSON.parse(req.body);
-
-    const { conversation } = await client.getOrCreateConversation({
-      channel: "channel",
-      tags: {
-        id: conversationId,
-      },
-    });
-
-    const { user } = await client.getOrCreateUser({
-      tags: {
-        id: userId,
-      },
-    });
-
-    const botpressMessage = {
-      tags: {
-        id: messageId,
-      },
-      type,
-      text,
-      userId: user.id,
-      conversationId: conversation.id,
-      payload,
-    };
-
-    if (!payload) {
-      botpressMessage.payload = { text: text };
-    } else if (!payload.text) {
-      botpressMessage.payload.text = text;
-    }
-
-    const { message } = await client.createMessage(botpressMessage);
-
-    logger
-      .forBot()
-      .debug(
-        `Done creating message in botpress for user ${userId}: ${JSON.stringify(
-          payload
-        )}`
-      );
-
-    return {
-      status: 200,
-      body: JSON.stringify({ message, user, conversation }),
-    };
-  },
+  handler: handleIncoming
 });
 
 export default integration;
